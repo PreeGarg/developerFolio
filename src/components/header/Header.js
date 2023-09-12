78a3661b1162b93c3651d74fd0004788e77e840f
PreeGarg <gargpreedhi@gmail.com>
2023-09-12 01:04:44 -0700

    Remove opensource

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,6 @@ import {
   greeting,
   workExperiences,
   skillsSection,
-  openSource,
   blogSection,
   talkSection,
   achievementSection,
@@ -17,7 +16,6 @@ import {
 function Header() {
   const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
-  const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
   const viewBlog = blogSection.display;
@@ -54,11 +52,6 @@ function Header() {
           {viewbigProjects && (
             <li>
               <a href="#projects">Projects</a>
-            </li>
-          )}
-          {viewOpenSource && (
-            <li>
-              <a href="#opensource">Open Source</a>
             </li>
           )}
           {viewAchievement && (
