088723766aad1f5fcdb42450daf6d30dad4de318
PreeGarg <gargpreedhi@gmail.com>
2023-09-12 19:41:43 -0700

    Build issues

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,3 @@
-import React, {useContext} from "react";
-import "./Footer.scss";
-import StyleContext from "../../contexts/StyleContext";
-
 export default function Footer() {
   return (
     <div></div>
